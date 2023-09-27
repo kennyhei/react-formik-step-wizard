0804e3f8bafeb424fe5c257c58d5f3f22cc06ea8
Kenny Heinonen <kenny_heinonen@hotmail.com>
2023-09-27 23:38:13 +0300

    Tweaks

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import Wizard from './wizard/Wizard'
 import { useWizard } from './helpers/hooks'
 import BasicFooter from './components/BasicFooter'
-import {
-  WizardValues as _WizardValues,
-  WizardContextValues as _WizardContextValues,
-  WizardProps as _WizardProps,
-  StepValues as _StepValues
+export type {
+  WizardValues,
+  WizardContextValues,
+  WizardProps,
+  StepValues
 } from './types'
-
-export type WizardValues = _WizardValues
-export type WizardContextValues = _WizardContextValues
-export type WizardProps = _WizardProps
-export type StepValues = _StepValues
 
 export { Wizard, useWizard, BasicFooter }
