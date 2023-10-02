150c5af422bb518969f9069748ba6c75705b9d1a
Kenny Heinonen <kenny_heinonen@hotmail.com>
2023-10-02 13:02:27 +0300

    Drop the idea of autogenerating ID for now

--- src/wizard/Wizard.tsx
+++ src/wizard/Wizard.tsx
@@ -30,14 +30,6 @@ function Wizard({
   wrapper,
   footer
 }: WizardProps) {
-  /*
-  TODO: Check if this makes any sense
-  steps = steps.map((step, idx) => {
-    if (step.id) { return step }
-    const type: any = step?.component?.type
-    return { ...step, id: type?.name || `step-${idx}` }
-  })
-  */
   let initialStep: Step = steps[0]
   let hashes = {}
   if (enableHash) {
